8c360ddb90802f7127dce55c14f00527c60b6f22
Aleksei <jojmusor@gmail.com>
2023-03-19 13:15:48 +0300

    made doReq func more universal, added request to service for receiving file info

--- storageService/ddownload.go
+++ storageService/ddownload.go
@@ -20,6 +20,7 @@ var (
 	urlGetFileInfo       = "https://api-v2.ddownload.com/api/file/info"
 	urlGetFilesList      = "https://api-v2.ddownload.com/api/file/list"
 	urlRenameFile        = "https://api-v2.ddownload.com/api/file/rename"
+	urlService           = "https://ddownload.com/"
 )
 
 type Service struct {
@@ -41,30 +42,44 @@ type UploadServerSummary struct {
 	Server string
 }
 
-func doRequestWQuery(method, path, queryParamName, queryParamVal string) (*http.Request, error) {
-	req, err := http.NewRequest(method, path, nil)
-	if err != nil {
-		return &http.Request{}, err
-	}
-	q := req.URL.Query()
-	q.Add(queryParamName, queryParamVal)
-	req.URL.RawQuery = q.Encode()
-	return req, nil
+type queryParams struct {
+	queryParamName string
+	queryParamVal  string
 }
 
-func doRequestWBody(method, path string, body *bytes.Buffer) (*http.Request, error) {
+type getFileInfoResponse struct {
+	Msg        string `json:"msg"`
+	ServerTime string `json:"server_time"`
+	Status     int    `json:"status"`
+	Result     []struct {
+		Uploaded string `json:"uploaded"`
+		Status   int    `json:"status"`
+		Filecode string `json:"filecode"`
+		Name     string `json:"name"`
+		Size     string `json:"size"`
+	} `json:"result"`
+}
+
+func doRequest(method, path string, queryParams []queryParams, body *bytes.Buffer) (*http.Request, error) {
 	req, err := http.NewRequest(method, path, body)
 	if err != nil {
 		return &http.Request{}, err
 	}
+
+	for _, v := range queryParams {
+		q := req.URL.Query()
+		q.Add(v.queryParamName, v.queryParamVal)
+		req.URL.RawQuery = q.Encode()
+	}
+
 	return req, nil
 }
 
 func (s Service) GetServerToUpload() (*UploadServerSummary, error) {
-	// Do request to server for getting actual server and session. Needed for upload file to correct server with session
-	req, err := doRequestWQuery(http.MethodGet, urlGetServerToUpload, apiKeyParamName, s.apiKey)
+	queryParams := []queryParams{{queryParamName: apiKeyParamName, queryParamVal: s.apiKey}}
+	req, err := doRequest(http.MethodGet, urlGetServerToUpload, queryParams, nil)
 	if err != nil {
-		return &UploadServerSummary{}, err
+		return nil, err
 	}
 
 	res, err := s.client.Do(req)
@@ -136,7 +151,7 @@ func (s Service) UploadFile(file []byte) (string, error) {
 		return "", err
 	}
 
-	req, err := doRequestWBody(http.MethodPost, uploadServerSummary.Server, body)
+	req, err := doRequest(http.MethodPost, uploadServerSummary.Server, []queryParams{}, body)
 	if err != nil {
 		return "", err
 	}
@@ -165,6 +180,28 @@ func (s Service) UploadFile(file []byte) (string, error) {
 
 	return uploadResp[0].FileCode, nil
 
+}
+
+func (s Service) getFileInfo() (*getFileInfoResponse, error) {
+	req, err := doRequest(http.MethodGet, urlGetFileInfo, []queryParams{{queryParamName: apiKeyParamName, queryParamVal: s.apiKey}}, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error in request of getFileLink func: %w", err)
+	}
+	res, err := s.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error in response of getFileLink func: %w", err)
+	}
+
+	defer res.Body.Close()
+	jsonRes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error in jsonresponse of getFileLink func: %w", err)
+	}
+
+	var getFileRes getFileInfoResponse
+	json.Unmarshal(jsonRes, &getFileRes)
+
+	return &getFileRes, nil
 }
 
 func New(apikey string) Service {
