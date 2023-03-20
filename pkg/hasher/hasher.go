7b8f8b2378366fed2d505848f41313a116f14570
Aleksei <jojmusor@gmail.com>
2023-03-20 23:20:42 +0300

    change behavior of hasher func, remove extra salt parameter

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -6,21 +6,18 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type Hasher struct {
-	salt string
-}
+type Hasher struct{}
 
-func New(salt string) *Hasher {
-	return &Hasher{salt: salt}
+func New() *Hasher {
+	return &Hasher{}
 }
 
 func (h Hasher) GetNewHash(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 	if err != nil {
 		return "", fmt.Errorf("failed generate hash, error: %w", err)
 	}
 	return string(hash), nil
-
 }
 
 func (h Hasher) CompareHashWithPassword(password, hash string) error {
