11efe0e4f18b20a792c27e5578ba547e035452e3
Aleksei <jojmusor@gmail.com>
2023-04-10 15:25:02 +0300

    added tests for: - GetBooksParamsFieldFiller - GetBooksPublic created helper func: - getReqWRecCtxParams changed naming prev helpers, remove error check from handler creater

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -39,7 +40,7 @@ func getMocks(t *testing.T) mocks {
 	}
 }
 
-func getRequestWRecJson(path, method string, buf *bytes.Buffer) (httptest.ResponseRecorder, echo.Context) {
+func getReqWRecJson(path, method string, buf *bytes.Buffer) (httptest.ResponseRecorder, echo.Context) {
 	serv := echo.New()
 	req := httptest.NewRequest(method, path, buf)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
@@ -52,7 +53,7 @@ func getRequestWRecJson(path, method string, buf *bytes.Buffer) (httptest.Respon
 	return *rec, c
 }
 
-func getRequestWRecFormFile(path, method string, file []byte) (httptest.ResponseRecorder, echo.Context) {
+func getReqWRecFormFile(path, method string, file []byte) (httptest.ResponseRecorder, echo.Context) {
 	serv := echo.New()
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
@@ -76,11 +77,11 @@ func Test_EchoHandlers_SignUp(t *testing.T) {
 	jwtTok := "askdlaksdlasd"
 	body, _ := json.Marshal(&inp)
 	buf := bytes.NewBuffer(body)
-	rec, c := getRequestWRecJson("/register", http.MethodPost, buf)
+	rec, c := getReqWRecJson("/register", http.MethodPost, buf)
 	mocks := getMocks(t)
 	mocks.serviceLayer.EXPECT().SignUp(inp).Return(jwtTok, nil)
 
-	h, _ := New(mocks.serviceLayer)
+	h := New(mocks.serviceLayer)
 	err := h.SignUp(c)
 
 	require.NoError(t, err)
@@ -96,12 +97,12 @@ func TestEchoHandlers_SignIn(t *testing.T) {
 	body, _ := json.Marshal(&inp)
 	buf := bytes.NewBuffer(body)
 
-	rec, c := getRequestWRecJson("/login", http.MethodPost, buf)
+	rec, c := getReqWRecJson("/login", http.MethodPost, buf)
 
 	mocks := getMocks(t)
 	jwtToken := "41351adas"
 
-	h, _ := New(mocks.serviceLayer)
+	h := New(mocks.serviceLayer)
 	mocks.serviceLayer.EXPECT().SignIn(inp).Return(jwtToken, nil)
 
 	err := h.SignIn(c)
@@ -120,10 +121,10 @@ func Test_UploadFile(t *testing.T) {
 	defer file.Close()
 	fileByte, err := io.ReadAll(file)
 	require.NoError(t, err)
-	rec, c := getRequestWRecFormFile("/files", http.MethodPost, fileByte)
+	rec, c := getReqWRecFormFile("/files", http.MethodPost, fileByte)
 	mocks.serviceLayer.EXPECT().UploadFile(fileByte).Return(fileToken, nil)
 
-	h, _ := New(mocks.serviceLayer)
+	h := New(mocks.serviceLayer)
 
 	err = h.UploadFile(c)
 	require.NoError(t, err)
@@ -176,8 +177,8 @@ func Test_CreateBook(t *testing.T) {
 	buf := bytes.NewBuffer(nil)
 	b, _ := json.Marshal(&reqBody)
 	buf.Write(b)
-	rec, c := getRequestWRecJson("/books", http.MethodPost, buf)
-	h, _ := New(mocks.serviceLayer)
+	rec, c := getReqWRecJson("/books", http.MethodPost, buf)
+	h := New(mocks.serviceLayer)
 
 	mocks.serviceLayer.EXPECT().GetUserByInsertedId(defaultUserId).Return(&userData, nil)
 	mocks.serviceLayer.EXPECT().CreateBook(reqBody.Title, reqBody.Description, reqBody.FileToken, userData.Email).Return(reqBody.FileToken, nil)
@@ -197,18 +198,87 @@ func Test_GetBook(t *testing.T) {
 		Description: "Terrible island stories",
 		FileURL:     "google.com",
 	}
-	rec, c := getRequestWRecJson(fmt.Sprintf("/books/%s", bookId), http.MethodGet, &bytes.Buffer{})
+	rec, c := getReqWRecJson(fmt.Sprintf("/books/%s", bookId), http.MethodGet, &bytes.Buffer{})
 	mocks := getMocks(t)
 
 	mocks.serviceLayer.EXPECT().GetBook(bookId).Return(&bookResp, nil)
 
-	h, _ := New(mocks.serviceLayer)
+	h := New(mocks.serviceLayer)
 
 	err := h.GetBook(c)
 	require.NoError(t, err)
 	var res *models.GetBookResponse
-	fmt.Println(rec.Body)
 	err = json.Unmarshal(rec.Body.Bytes(), &res)
 	require.NoError(t, err)
 	assert.Equal(t, bookResp, *res)
+}
+
+func Test_GetBooksParamsFieldFiller(t *testing.T) {
+	wantFilt := models.Filter{
+		Email:  userEmail,
+		Search: "asdjkasjdkajs",
+	}
+	wantSort := models.Sort{
+		SortField: "title",
+		Limit:     5,
+		Direction: "desc",
+		Offset:    0,
+	}
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/books?search=%s&limit=%d&sort=%s&direction=%s", wantFilt.Search, wantSort.Limit, wantSort.SortField, wantSort.Direction), nil)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	filter, sorting, err := getBooksParamsFieldFiller(c, userEmail)
+	require.NoError(t, err)
+	assert.Equal(t, true, reflect.DeepEqual(&wantFilt, filter))
+	assert.Equal(t, true, reflect.DeepEqual(&wantSort, sorting))
+}
+
+func getReqWRecCtxParams(wantFilt models.Filter, wantSort models.Sort) (*httptest.ResponseRecorder, *echo.Context) {
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/books?search=%s&limit=%d&sort=%s&direction=%s", wantFilt.Search, wantSort.Limit, wantSort.SortField, wantSort.Direction), nil)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	return rec, &c
+}
+
+func Test_GetBooksPublic(t *testing.T) {
+	wantFilt := models.Filter{
+		Email:  "",
+		Search: "asdjkasjdkajs",
+	}
+	wantSort := models.Sort{
+		SortField: "title",
+		Limit:     5,
+		Direction: "desc",
+		Offset:    0,
+	}
+	wantBooks := []models.BookData{
+		{
+			Id:          "1",
+			Title:       "Title1",
+			Description: "description1",
+			FileToken:   "FileToken1",
+			Url:         "URL1",
+			OwnerEmail:  "email1",
+		},
+		{
+			Id:          "2",
+			Title:       "Title2",
+			Description: "description2",
+			FileToken:   "FileToken2",
+			Url:         "URL2",
+			OwnerEmail:  "email2",
+		},
+	}
+
+	rec, c := getReqWRecCtxParams(wantFilt, wantSort)
+
+	mocks := getMocks(t)
+	mocks.serviceLayer.EXPECT().GetBooks(wantFilt, wantSort).Return(&wantBooks, nil)
+	h := New(mocks.serviceLayer)
+	err := h.GetBooksPublic(*c)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
+	var received []models.BookData
+	json.Unmarshal(rec.Body.Bytes(), &received)
+	assert.Equal(t, true, reflect.DeepEqual(wantBooks, received))
 }
